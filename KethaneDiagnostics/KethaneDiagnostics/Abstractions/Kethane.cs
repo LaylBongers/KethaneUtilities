30e3e45306ae665f449e55c73f2f48bce66915db
Layl Conway <laylconway@live.com>
2013-07-27 02:44:49 +0200

    Added KethaneDiagnostics files.

---
+++ KethaneDiagnostics/KethaneDiagnostics/Abstractions/Kethane.cs
@@ -0,0 +1,39 @@
+﻿using KethaneDiagnostics.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KethaneDiagnostics.Abstractions
+{
+	class Kethane
+	{
+		// Logger
+		Logger logger = new Logger("KSP");
+
+		// Abstractions
+		public KSP KSP { get; set; }
+
+		public Kethane(KSP ksp)
+		{
+			KSP = ksp;
+		}
+
+		public void Load()
+		{
+			ModFolder kethaneFolder = null;
+			foreach(ModFolder folder in KSP.ModFolders)
+			{
+				if (folder.Path.Contains("Kethane"))
+				{
+					kethaneFolder = folder;
+					break;
+				}
+			}
+			if (kethaneFolder == null)
+				throw new Exception("Kethane folder could not be found in GameData, you installed Kethane incorrectly.");
+			logger.Log("Kethane Folder Found");
+		}
+	}
+}
---
+++ KethaneDiagnostics/KethaneDiagnostics/Program.cs
@@ -0,0 +1,73 @@
+﻿using KethaneDiagnostics.Abstractions;
+using KethaneDiagnostics.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KethaneDiagnostics
+{
+	class MainWindow
+	{
+		// Logger
+		static Logger logger = new Logger("MainWindow");
+
+		[STAThread]
+		static void Main(string[] args)
+		{
+			try {
+				MainWindow mainWindow = new MainWindow();
+				mainWindow.Run();
+			}
+			catch (Exception e)
+			{
+				logger.ResetIndention();
+				logger.LogException(e, Level.Error);
+			}
+
+			// Make sure the user knows the utility is done.
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine("\nKethaneDiagnostics finished running.");
+			Console.WriteLine("Please remember that this tool stops as soon as an error occurs.");
+			Console.WriteLine("A problem may not have been detected as a result of this.");
+			Console.WriteLine("Some unknown or hard to detect problems may also not have been detected.");
+			Console.WriteLine("Press enter to exit...");
+			while (Console.ReadKey(true).Key != ConsoleKey.Enter) ;
+		}
+
+		// Abstractions
+		KSP ksp;
+		Kethane kethane;
+
+		public void Run()
+		{
+			/// Load in and scan KSP's files ///
+			logger.BeginBlock("Scanning KSP");
+			
+			// Ask user to browse to the KSP folder
+			FolderBrowserDialog kspFolderDialog = new FolderBrowserDialog();
+			kspFolderDialog.RootFolder = Environment.SpecialFolder.ProgramFilesX86;
+			kspFolderDialog.ShowNewFolderButton = false;
+			kspFolderDialog.Description = "Please select the KSP Root folder. This is the folder containing \"KSP.exe\". If you use steam this folder will be in \"Steam\\SteamApps\\common\".";
+			DialogResult kspFolderResult = kspFolderDialog.ShowDialog();
+			if(kspFolderResult != DialogResult.OK)
+				throw new Exception("User closed KSP browse dialog.");
+
+			// Load and Scan the KSP Installation
+			ksp = new KSP(kspFolderDialog.SelectedPath);
+			ksp.Load();
+
+			logger.EndBlock("Scanning KSP");
+
+			/// Load in and scan Kethane's files ///
+			logger.BeginBlock("Scanning Kethane");
+
+			kethane = new Kethane(ksp);
+			kethane.Load();
+
+			logger.EndBlock("Scanning Kethane");
+		}
+	}
+}
